82bad01cf1b04f13deb12287987df14180571d29
Max Lowery <brainlessml@gmail.com>
2016-09-27 17:48:13 -0500

    :baby_chick: Made some programs to test the current robot and the servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
@@ -1,15 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by mlowery2 on 9/21/2016.
  */
 
-@Autonomous(name = "Testing Opmode", group = "Testing")
+
+@TeleOp(name="Basic Teleop", group="Testing")
 
 public class BasicTeleop extends LinearOpMode {
 
@@ -18,15 +20,53 @@ public class BasicTeleop extends LinearOpMode {
     DcMotor right1;
     DcMotor right2;
 
+    Servo rightBp;
+    Servo leftBp;
+
+    final float DEADZONE = 0.200f;
+    final double BPMIN = 0;
+    final double BPMAX = 1; // divide by 2 for mid
+
     public void runOpMode() throws InterruptedException {
 
-        left1 = hardwareMap.dcMotor.get("left1");
-        left2 = hardwareMap.dcMotor.get("left2");
+        left1  = hardwareMap.dcMotor.get("left1");
+        left2  = hardwareMap.dcMotor.get("left2");
         right1 = hardwareMap.dcMotor.get("right1");
         right2 = hardwareMap.dcMotor.get("right2");
 
+        left1.setDirection(DcMotor.Direction.REVERSE); // make sure this works
+        left2.setDirection(DcMotor.Direction.REVERSE);
 
-    }
+        rightBp = hardwareMap.servo.get("rightBp");
+        leftBp  = hardwareMap.servo.get("leftBp");
+
+        gamepad1.setJoystickDeadzone(DEADZONE);
+        gamepad2.setJoystickDeadzone(DEADZONE);
+
+        rightBp.setPosition(BPMAX/2); // init to default positions
+        leftBp.setPosition(BPMAX/2);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            moveRight(gamepad1.right_stick_y, -1);
+            moveLeft(gamepad1.left_stick_y, -1);
+
+            if(gamepad1.b)
+            {
+                rightBp.setPosition(BPMAX);
+                leftBp.setPosition(BPMAX);
+            }
+            else if(gamepad1.x)
+            {
+                rightBp.setPosition(BPMIN);
+                leftBp.setPosition(BPMIN);
+            }
+
+        }
+    } // End of running code
+     //  Start of Functions
 
     public void moveLeft(float speed, int dist)
     {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTeleop.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+/**
+ * Created by mlowery2 on 9/27/2016.
+ */
+@TeleOp(name="Servo Teleop", group="Testing")
+public class ServoTeleop extends LinearOpMode { // Just to find the min max positions of any servo
+    Servo test;
+    double pos = 0.5;
+
+    public void runOpMode() throws InterruptedException
+    {
+        test = hardwareMap.servo.get("left");
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            if(gamepad1.x && pos<1)
+            {
+                pos += 0.01;
+            }
+            else if(gamepad1.b && pos>0)
+            {
+                pos -= 0.01;
+            }
+
+            telemetry.addData("Pos", ""+pos); // hopfully I didnt forget how this works
+            test.setPosition(pos);
+            Thread.sleep(100);
+        }
+    }
+}
