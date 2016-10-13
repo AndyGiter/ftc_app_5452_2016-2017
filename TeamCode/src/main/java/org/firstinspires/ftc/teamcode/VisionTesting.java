1c78105e5b3e6386135344a1cdfc79fbf52f64b5
Max Lowery <brainlessml@gmail.com>
2016-10-13 16:49:47 -0500

    :japanese_goblin: Created a file to play around with the camera

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTesting.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by mlowery2 on 10/13/2016.
+ *
+ * This code will at first take heavy inspiration from some programs in bchay/3785-RESQ-Code but mostly CameraPreview.java. Thanks to bchay.
+ */
+
+@TeleOp(name="Vision Testing", group="Testing")
+public class VisionTesting extends LinearOpMode {
+
+    public void runOpMode()
+    {
+
+    }
+}
