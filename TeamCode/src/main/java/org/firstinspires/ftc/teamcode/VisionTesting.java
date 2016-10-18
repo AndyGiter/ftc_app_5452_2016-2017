095f324a76221eafd3170b35887954c02e1b4c39
Max Lowery <brainlessml@gmail.com>
2016-10-18 17:28:21 -0500

    :skull: Updated VisionTesting.java to work with CameraPreview.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTesting.java
@@ -1,17 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.hardware.Camera;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-import java.io.IOException;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.CameraPreview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Camera;
+import android.view.SurfaceView;
 
 /**
  * Created by mlowery2 on 10/13/2016.
@@ -22,8 +20,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="Vision Testing", group="Testing")
 public class VisionTesting extends LinearOpMode {
 
-    public void runOpMode()
+    public void runOpMode() throws InterruptedException
     {
+        CameraPreview c = new CameraPreview((SurfaceView) ((Activity) hardwareMap.appContext).findViewById(R.id.surfaceView));
+        c.start();
 
+        waitForStart();
+
+        c.capture();
+        telemetry.addData("Color Detected", "RGB: " + c.colorDetected());
+        telemetry.addData("Color Detected", "HSV: " + c.colorDetectedHSV());
+        telemetry.update();
+        while(opModeIsActive()){Thread.sleep(50);}
     }
 }
