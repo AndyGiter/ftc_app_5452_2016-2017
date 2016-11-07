091700d17fa9f7b4175c44f0b24962d125dfb8cb
Max Lowery <brainlessml@gmail.com>
2016-11-07 16:49:00 -0600

    :tangerine: Created a program to test if servo.getPosition is read or a stored value

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by mlowery2 on 11/7/2016.
+ */
+@Autonomous(name = "Servo Testing", group ="testing")
+public class servoTest extends LinearOpMode {
+
+    Servo cannon;
+    public void runOpMode() throws InterruptedException
+    {
+
+        cannon.setPosition(1);
+
+        telemetry.addData("Current Position: ", cannon.getPosition());
+        telemetry.update();
+
+        waitForStart();
+
+        cannon.setPosition(0);
+
+        while(opModeIsActive())
+        {
+            telemetry.addData("Current Position: ", cannon.getPosition());
+            telemetry.update();
+        }
+    }
+}
