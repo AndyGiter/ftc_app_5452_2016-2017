166640f461b7a960bfa0176f7295a1e81a6f454f
Max Lowery <brainlessml@gmail.com>
2016-12-01 20:33:18 -0600

    :taxi: Added telemetry functions to SensorTester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTester.java
@@ -2,13 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import android.widget.Switch;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 /**
  * Created by mlowery2 on 11/22/2016.
  */
 
+//TODO: finish this shit
 @Autonomous(name = "Sensor tester", group = "diagnostic")
 public class SensorTester extends LinearBase {
 
@@ -16,12 +23,19 @@ public class SensorTester extends LinearBase {
 
     public void runOpMode() throws InterruptedException
     {
-        initalize(DcMotor.RunMode.RUN_TO_POSITION, true);
+        initalize(true);
+
+        red.enableLed(true);
+        blue.enableLed(true);
         waitForStart();
 
-        while(opModeIsActive())
-        {
-            changeLightOn();
+        while(opModeIsActive()) {
+            updateHsv();
+            colorTelemetry(red, hsvValuesRed[0]);
+            colorTelemetry(blue, hsvValuesBlue[0]);
+            telemetry.update();
+
+            Thread.sleep(30);
         }
     }
 
@@ -35,7 +49,7 @@ public class SensorTester extends LinearBase {
         if(sensorNum < 4)
             sensorNum += 1;
         else
-            sensorNum = 0;
+            sensorNum = 1;
 
         switch(sensorNum)
         {
@@ -53,5 +67,29 @@ public class SensorTester extends LinearBase {
                 break;
 
         }
+    }
+
+    public void gyroTelemetry(ModernRoboticsI2cGyro sensorGyro)
+    {
+
+        telemetry.addData("Gyro: ", sensorGyro.getIntegratedZValue() + "");
+        telemetry.addData("Gyro Heading: ", sensorGyro.getHeading() + "");
+
+    }
+
+    public void rangeTelemety(ModernRoboticsI2cRangeSensor rangeSensor)
+    {
+        telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic());
+        telemetry.addData("raw optical", rangeSensor.rawOptical());
+        telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
+        telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM)); // what to use
+    }
+
+    public void colorTelemetry(ColorSensor color, float hsvVals)
+    {
+        telemetry.addData("Red: ", color.red());
+        telemetry.addData("Blue: ", color.blue());
+        telemetry.addData("Green: ", color.green());
+        telemetry.addData("HSV", hsvVals);
     }
 }
