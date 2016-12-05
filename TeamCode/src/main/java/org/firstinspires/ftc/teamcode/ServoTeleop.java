65f274728ea9cc82a9a74325e06314fc9c005a65
Max Lowery <brainlessml@gmail.com>
2016-12-05 08:41:15 -0600

    Changed ServoTeleop for the cannon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTeleop.java
@@ -2,23 +2,22 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 /**
  * Created by mlowery2 on 9/27/2016.
  */
 @TeleOp(name="Servo Teleop", group="Testing")
-public class ServoTeleop extends LinearOpMode { // Just to find the min max positions of any servo
-    Servo test;
+public class ServoTeleop extends LinearBase { // Just to find the min max positions of any servo
     double pos = 0;
     final double changeBy = 0.01;
     final long waitTime = 100;
 
+    boolean ledState = true;
+
     public void runOpMode() throws InterruptedException
     {
-        test = hardwareMap.servo.get("cannon");
-
-        telemetry.addData("Revision", "4");
-        telemetry.update();
+        initalize(true);
 
         waitForStart();
 
@@ -33,9 +32,11 @@ public class ServoTeleop extends LinearOpMode { // Just to find the min max posi
                 pos -= changeBy;
             }
 
-            telemetry.addData("Servo Position", "%5.2f", pos);
+            updateHsv();
+
+            telemetry.addData("Servo Position ", "%5.2f", pos);
             telemetry.update();
-            test.setPosition(pos);
+            cannon.setPosition(pos);
             Thread.sleep(waitTime);
         }
     }
