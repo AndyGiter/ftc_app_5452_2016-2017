8fb4abd0131430ae43cea596f3822da84e95a836
Max Lowery <brainlessml@gmail.com>
2017-01-11 15:56:28 -0600

    :dragon: Updated the Tele-op to the current robot
    This is not the last revision of this program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
@@ -20,15 +20,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 * Controls
 * Right Joystick: Right Motors
 * Left Joystick: Left Motors
-* A: Old ball shooter
+* A: Shoot 1 ball (2 motor rotations)
+* Right Trigger: Collect (not toggled)
+* Left Trigger: Collect Backwards
+* Y: Drive train Slow Mode
+* Dpad Right: Lower ball holder
+* Dpad LeftL: Raise ball holder
 *
-* TODO:
-* RT: Ball collector
-* A: Ball Shooter
-* DPad up: Forward Mode
-* DPad down: Backward Mode
-* DPad left: Slow
-* DPad right: Fast
 * */
 
 @TeleOp(name="Basic Teleop", group="Teleop")
@@ -37,44 +35,91 @@ public class BasicTeleop extends LinearBase {
 
     private final double SLOW_MOD = 0.3; // 30% of normal speed
     private boolean slow = false;
-    private Direction currentDirection = Direction.FORWARD;
+    private boolean press = false;
+    private int newPressTime = 0;
 
     public void runOpMode() throws InterruptedException
     {
 
-        initalize(DcMotor.RunMode.RUN_USING_ENCODER);
+        initalize(DcMotor.RunMode.RUN_USING_ENCODER, true);
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
 
         while(opModeIsActive())
         {
-            if(gamepad1.a) // TODO: Update when new ball shooter is on the robot
+            /*
+             if(gamepad1.b)
             {
-                cannon.setPosition(0);
+                shooter.setPower(-0.75);
             }
 
-            if(gamepad1.dpad_left && !slow)
+            if(shooter.getCurrentPosition() <= shooter.getTargetPosition()) // make a better system to control the shooter
             {
-                slow = true;
+                if(gamepad1.a)
+                {
+                    //shooter.setMode(DcMotor.RunMode.RUN_TO_POSITION); // at some point test how much lag this creates
+
+                    shooter.setTargetPosition(shooter.getCurrentPosition() + (-3395)); // make sure this spins the right way
+                    shooter.setPower(0.9);
+                }
+
+                else
+                {
+                    shooter.setPower(0);
+                }
             }
-            else if(gamepad1.dpad_right && slow)
+            */
+
+            if(gamepad2.a)
             {
-                slow = false;
+                shooter.setPower(-0.9);
+            }
+            else
+            {
+                shooter.setPower(0);
             }
 
-            if(gamepad1.dpad_up && currentDirection == Direction.BACKWARD)
+
+            if(gamepad2.right_bumper)
             {
-                currentDirection = Direction.FORWARD;
+                collector.setPower(-0.9);
             }
-            else if(gamepad1.dpad_down && currentDirection == Direction.FORWARD)
+            else if(gamepad2.left_bumper)
             {
-                currentDirection = Direction.BACKWARD;
+                collector.setPower(0.9);
+            }
+            else
+            {
+                collector.setPower(0);
             }
 
-            left1.setPower(-1 * gamepad1.right_stick_y);
-            left2.setPower(-1 * gamepad1.right_stick_y);
-            right1.setPower(-1 * gamepad1.left_stick_y);
-            right2.setPower(-1 * gamepad1.left_stick_y);
+            if(gamepad1.y)
+            {
+                slow = !slow;
 
+                press = true;
+            }
+
+            right1.setPower(-1 * gamepad1.right_stick_y * (slow?SLOW_MOD:1));
+            right2.setPower(-1 * gamepad1.right_stick_y * (slow?SLOW_MOD:1));
+            left1.setPower(-1 * gamepad1.left_stick_y * (slow?SLOW_MOD:1));
+            left2.setPower(-1 * gamepad1.left_stick_y * (slow?SLOW_MOD:1));
+
+            if(press) // make a better system for this that deosn't prevent use of the joysticks
+            {
+                Thread.sleep(200);
+                press = false;
+            }
+
+            if(verbose)
+            {
+                telemetry.addData("Shooter is busy?", shooter.isBusy());
+                telemetry.addData("Current Shooter Pos", shooter.getCurrentPosition());
+                telemetry.addData("Shooter Target Pos", shooter.getTargetPosition());
+            }
+
+            telemetry.addData("Is slow mode on", slow);
+            telemetry.update();
         }
     }
 }
