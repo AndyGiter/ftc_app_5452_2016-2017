1cd72c2c0bedb121bfb7e42b0fee9dd849b096e9
Max Lowery <brainlessml@gmail.com>
2017-01-12 19:22:13 -0600

    :radio: Finalized Tele-op for league 3
    :smile:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
@@ -15,11 +15,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 *       Y: Toggle Slow Mode
 *       Right Joystick: Right Motor
 *       Left Joystick: Left Motors
+*       Start: Toggle Logging
 *
 *   Gamepdad2:
 *       A: Spin the Snail Cam (One rotation is a shot)
 *       Right Bumper: Collect inwards
 *       Left Bumpers: Push out balls
+*       Start: Toggle Logging
 * */
 
 @TeleOp(name="Basic Teleop", group="Teleop")
@@ -29,31 +31,59 @@ public class BasicTeleop extends LinearBase {
     private boolean slow = false;
     private boolean press = false;
 
+    private DcMotor.RunMode shootMode = DcMotor.RunMode.RUN_TO_POSITION;
+
     public void runOpMode() throws InterruptedException
     {
 
         initalize(DcMotor.RunMode.RUN_USING_ENCODER);
-        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter.setMode(shootMode);
         waitForStart();
 
         while(opModeIsActive())
         {
-
-            if(gamepad2.a) // TODO: Make a better shooting system that allows for one press shoot and reload and manual control
+            if(gamepad1.start || gamepad2.start) // an emergency button to on the fly add logging
             {
-                shooter.setPower(-0.9);
-            }
-            else
-            {
-                shooter.setPower(0);
+                verbose = !verbose;
+                press = true;
             }
 
+             if(shooter.getCurrentPosition() <= shooter.getTargetPosition()) // the inequality is negative because the motor goes backwards
+             {
+                if(gamepad1.a)
+                {
+                    if(shootMode != DcMotor.RunMode.RUN_TO_POSITION)
+                    {
+                        shootMode = DcMotor.RunMode.RUN_TO_POSITION;
+                        shooter.setMode(shootMode);
+                    }
 
-            if(gamepad2.right_bumper)
+                    shooter.setTargetPosition(shooter.getCurrentPosition() + (-3390));
+                    shooter.setPower(-0.9);
+                }
+
+                else if(gamepad1.b)
+                {
+                    if(shootMode != DcMotor.RunMode.RUN_USING_ENCODER)
+                    {
+                        shootMode = DcMotor.RunMode.RUN_USING_ENCODER;
+                        shooter.setMode(shootMode);
+                    }
+
+                    shooter.setPower(-0.9);
+                }
+
+                else
+                {
+                    shooter.setPower(0);
+                }
+            }
+
+            if(gamepad1.right_bumper)
             {
                 collector.setPower(-0.9);
             }
-            else if(gamepad2.left_bumper)
+            else if(gamepad1.left_bumper)
             {
                 collector.setPower(0.9);
             }
@@ -74,21 +104,20 @@ public class BasicTeleop extends LinearBase {
             left1.setPower(-1 * gamepad1.left_stick_y * (slow?SLOW_MOD:1));
             left2.setPower(-1 * gamepad1.left_stick_y * (slow?SLOW_MOD:1));
 
-            if(press) // TODO: make a better system for this that deosn't prevent use of the joysticks
-            {
-                Thread.sleep(250);
-                press = false;
-            }
-
+            telemetry.addData("Is slow mode on", slow);
             if(verbose)
             {
                 telemetry.addData("Shooter is busy?", shooter.isBusy());
                 telemetry.addData("Current Shooter Pos", shooter.getCurrentPosition());
                 telemetry.addData("Shooter Target Pos", shooter.getTargetPosition());
             }
-
-            telemetry.addData("Is slow mode on", slow);
             telemetry.update();
+
+            if(press) // TODO: make a better system for this that deosn't prevent use of the joysticks
+            {
+                Thread.sleep(250);
+                press = false;
+            }
         }
     }
 }
