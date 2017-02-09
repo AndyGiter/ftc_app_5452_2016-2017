4542e07034ecbb53a2f14f76fce2f57e92fbd8b6
Max Lowery <brainlessml@gmail.com>
2017-02-08 18:37:46 -0600

    Played around with the new gyro
    We are not going to use it because the range is just -180 to 180 or -360
    to 0 and the modern robotics gyro just adds up or down without reseting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroThrowdown.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroThrowdown.java
@@ -26,109 +26,81 @@ import java.util.Locale;
  */
 @Autonomous(name = "GYRO SHOWDOWN", group = "Sensor")
 public class GyroThrowdown extends LinearBase {
+    //----------------------------------------------------------------------------------------------
+    // State
+    //----------------------------------------------------------------------------------------------
 
-    // The new, untested gyro sensor
+    // The IMU sensor object
     BNO055IMU imu;
 
+    // State used for updating telemetry
     Orientation angles;
     Acceleration gravity;
 
-    public void runOpMode() throws InterruptedException
-    {
+    //----------------------------------------------------------------------------------------------
+    // Main logic
+    //----------------------------------------------------------------------------------------------
+
+    @Override public void runOpMode() throws InterruptedException {
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode and do this at some point maybe
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
         parameters.loggingEnabled      = true;
         parameters.loggingTag          = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
 
+        angles   = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
+        gravity  = imu.getGravity();
+
+        // Wait until we're told to go
         initalize(true);
         waitForStart();
-        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000); // Make sure this is in the right spot
-        Thread.sleep(100);
 
-        turnG(0.45, 90);
+        // Start the logging of measured acceleration
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        // Loop and update the dashboard
 
         while(opModeIsActive())
         {
-            telemetry.addData("Gyro heading: ", gyro.getIntegratedZValue()+"");
-            telemetry.addData("IMU heading: ", getStringHeading());
+            if(gamepad1.a)
+                turn(0.45, 90);
+            else if(gamepad1.b)
+                turnIMU(0.45, 90);
+
+            right1.setPower(-1 * gamepad1.right_stick_y);
+            right2.setPower(-1 * gamepad1.right_stick_y);
+            left1.setPower(-1 * gamepad1.left_stick_y);
+            left2.setPower(-1 * gamepad1.left_stick_y);
+
+            angles   = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
+            telemetry.addData("Gyro Heading", gyro.getIntegratedZValue());
+            telemetry.addData("IMU Heading (string)", formatAngle(angles.angleUnit, angles.firstAngle));
+            telemetry.addData("IMU Heading (pre string)", AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle)));
+            telemetry.addData("IMU Heading (pre norm)", AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle));
+            telemetry.addData("IMU Angle 1", angles.firstAngle);
             telemetry.update();
         }
 
     }
 
-    public void turnG(double maxSpeed, int deg) throws InterruptedException
-    {
-        final int TURN_RANGE = 6;
-        int targetHeading = gyro.getIntegratedZValue() + deg;
-
-        if(defualtRunMode != DcMotor.RunMode.RUN_USING_ENCODER)
-        {
-            right1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            right2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            left2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            left1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-
-        // While the gyro is not within the range (TURN_RANGE)
-        while(!(gyro.getIntegratedZValue() <= targetHeading+(TURN_RANGE/2.0) && gyro.getIntegratedZValue() >= targetHeading-(TURN_RANGE/2.0)) && opModeIsActive())
-        {
-
-            if(gyro.getIntegratedZValue() > targetHeading) // Right turn
-            {
-                left1.setPower(maxSpeed);
-                left2.setPower(maxSpeed);
-                right1.setPower(-1 * maxSpeed);
-                right2.setPower(-1 * maxSpeed);
-            }
-
-            else //then its a left turn
-            {
-                left1.setPower(-1 * maxSpeed);
-                left2.setPower(-1 * maxSpeed);
-                right1.setPower(maxSpeed);
-                right2.setPower(maxSpeed);
-            }
-
-            if(verbose)
-            {
-                telemetry.addData("Gyro target: ", targetHeading+"");
-                telemetry.addData("Gyro heading: ", gyro.getIntegratedZValue()+"");
-                telemetry.addData("IMU heading: ", getStringHeading());
-                telemetry.addData("Robot speed: ", maxSpeed+"");
-                telemetry.update();
-            }
-        }
-
-        right1.setPower(0);
-        right2.setPower(0);
-        left1.setPower(0);
-        left2.setPower(0);
-
-        if(defualtRunMode != DcMotor.RunMode.RUN_USING_ENCODER) // last thing to do
-        {
-            right1.setMode(defualtRunMode);
-            right2.setMode(defualtRunMode);
-            left2.setMode(defualtRunMode);
-            left1.setMode(defualtRunMode);
-        }
-
-        Thread.sleep(300);
-
-        if(verbose){telemetry.addData("Done: ", "Turning. " + deg + " deg. Heading: " + gyro.getIntegratedZValue()); telemetry.update();}
-
-    }
 
     // Make sure that negative is still right and if not change all the code i guess idk.
     public void turnIMU(double maxSpeed, int deg) throws InterruptedException
     {
         final int TURN_RANGE = 6;
-        int targetHeading = gyro.getIntegratedZValue() + deg;
+        double targetHeading = getHeading() + deg;
 
         if(defualtRunMode != DcMotor.RunMode.RUN_USING_ENCODER)
         {
@@ -142,7 +114,7 @@ public class GyroThrowdown extends LinearBase {
         while(!(getHeading() <= targetHeading+(TURN_RANGE/2.0) && getHeading() >= targetHeading-(TURN_RANGE/2.0)) && opModeIsActive())
         {
 
-            if(gyro.getIntegratedZValue() > targetHeading) // Right turn
+            if(getHeading() > targetHeading) // Right turn
             {
                 left1.setPower(maxSpeed);
                 left2.setPower(maxSpeed);
@@ -183,13 +155,14 @@ public class GyroThrowdown extends LinearBase {
 
         Thread.sleep(300);
 
-        if(verbose){telemetry.addData("Done: ", "Turning. " + deg + " deg. Heading: " + gyro.getIntegratedZValue()); telemetry.update();}
+        if(verbose){telemetry.addData("Done", "Turning. " + deg + " deg. Heading: " + getHeading()); telemetry.update();}
 
     }
 
     private double getHeading() // Hopefully this works
     {
-        return AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle);
+        angles   = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
+        return AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle));
     }
 
     private String getStringHeading()
