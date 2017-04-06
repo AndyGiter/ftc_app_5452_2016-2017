b0fde5e3a51204dad6856197c15ef7443a261b1e
Max Lowery <brainlessml@gmail.com>
2017-04-06 16:57:15 -0500

    Created a program to do fuse testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/moveTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by mlowery2 on 4/6/2017.
+ */
+
+@Autonomous(name = "Move Test", group="testing")
+public class moveTest extends LinearBase {
+
+    public void runOpMode() throws InterruptedException
+    {
+        initalize(DcMotor.RunMode.RUN_TO_POSITION, true);
+        waitForStart();
+        Thread.sleep(100);
+
+        move(MAX_MOVE_SPEED, 1440);
+    }
+}
